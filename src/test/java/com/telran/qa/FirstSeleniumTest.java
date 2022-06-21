0973c630b765593e0d673732ecd37fa4fe120522
Ira Baker <49585685+irinabaker@users.noreply.github.com>
2022-06-21 12:40:34 +0200

    Added FirstFindElementTests

--- src/test/java/com/telran/qa/FirstSeleniumTest.java
+++ src/test/java/com/telran/qa/FirstSeleniumTest.java
@@ -2,8 +2,12 @@ package com.telran.qa;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class FirstSeleniumTest {
 
@@ -13,13 +17,37 @@ public class FirstSeleniumTest {
     @BeforeMethod
     public void setUp() {
         driver = new ChromeDriver();
+       // driver = new FirefoxDriver();
        // System.setProperty("webdriver.chrome.driver","C:\\Tools\\chromedriver.exe");
-        driver.get("https://www.google.com");
+       // driver.get("https://www.google.com");
+        driver.navigate().to("https://www.google.com");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
     //Test
     @Test
     public void openGoogleTest() {
-        System.out.println("Site opened!");
+       /// System.out.println("Site opened!");
     }
-    //after - tearDawn
+
+    @Test
+    public void navigateTest() {
+        //go back
+        driver.navigate().back();
+        //go forward
+        driver.navigate().forward();
+        //refresh the page
+        driver.navigate().refresh();
+    }
+
+    //after - tearDown
+    @AfterMethod(enabled = false)
+    public void tearDown() {
+        //close browser
+        //all tabs
+        driver.quit();
+        //one tab
+      //  driver.close();
+    }
+
 }
